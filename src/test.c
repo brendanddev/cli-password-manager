ccc2752ee8686287bea4a2af87b63d84089748e1
Brendan Dileo <dileob23@gmail.com>
2026-01-10 16:26:28 -0500

    Continue working on add/view functions

--- src/test.c
+++ src/test.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <math.h>
 
+bool add_password(char *input);
+bool view_password(char *type);
 
 int main(void) {
     
@@ -18,6 +20,26 @@ int main(void) {
     fgets(input, 256, stdin);
     printf("You entered: %s", input);
 
+    if (add_password(input)) {
+        printf("Successfully wrote the password.\n");
+    } else {
+        printf("Failed to write the password.\n");
+    }
+
+    // Free memory allocated for input string
+    free(input);
+    input = NULL;
+
+
+    printf("Enter the type of password to view: \n");
+
+    char *type = NULL;
+    type = malloc(100 * sizeof(char));
+    if (type == NULL) return -1;
+
+    fgets(type, 100, stdin);
+    printf("You entered: %s", type);
+
     // Creates pointer to the first substring seperated by the delimeter
     char *token = strtok(input, ",");
 
@@ -27,7 +49,49 @@ int main(void) {
         token = strtok(NULL, ",");      // Get next token
     }
 
-    free(input);
-    input = NULL;
+    free(type);
+    type = NULL;
+
+
     return 0;
+}
+
+
+// Adds a new password entry to the csv file
+bool add_password(char *input) {
+
+    // Create a file pointer to the opened file
+    FILE *fptr = NULL;
+    fptr = fopen("passwords.csv", "a");
+    if (fptr == NULL) return false;
+
+    // Writes the input text to the file
+    fprintf(fptr, input);
+
+    // Close the open file and nullify pointer
+    fclose(fptr);
+    fptr = NULL;
+    return true;
+}
+
+// View a password by type
+bool view_password(char *type) {
+
+    FILE *fptr = NULL;
+    fptr = fopen("passwords.csv", "r");
+    if (fptr == NULL) return false;
+
+    // char *token = strtok(fptr, ",");
+
+    // // Loop through each token
+    // while (token != NULL) {
+    //     printf("%s\n", token);
+    //     token = strtok(NULL, ",");      // Get next token
+    // }
+
+
+    // Close the open file and nullify pointer
+    fclose(fptr);
+    fptr = NULL;
+    return true;
 }
