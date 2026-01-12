2509bc756508ca32f1cc4820a2ee82ee2fa38105
Brendan Dileo <dileob23@gmail.com>
2026-01-12 17:38:27 -0500

    Test with XOR encryption

--- src/test.c
+++ src/test.c
@@ -5,5 +5,23 @@
 #include <math.h>
 
 int main(void) {
+
+    // Each char is 1 byte (8 bits), and for each character in the provided string,
+    // XOR is applied bit by bit, resulting in a potentially new character, and scrambled string
+    char test[30] = "Hello World";
+    char key = 0x55;
+
+    // Applies XOR to each of the 8 bits
+    for (int i = 0; i < strlen(test); i++) {
+        test[i] = test[i] ^ key;
+        printf("%c \n", test[i]);
+    }
+
+    for (int i = 0; i < strlen(test); i++) {
+        test[i] = test[i] ^ key;
+        printf("%c \n", test[i]);
+    }
+
+
     return 0;
 }
